542b1d6483b383ac2f42c4c60fbeaeb65044d65b
jac-24 <jac524@ic.ac.uk>
2026-05-26 10:58:58 +0100

    Finished proof on zero locus being an affine variety

--- FirstLean/algebraicgeometry/Algebraicgeometry/Chapter2.lean
+++ FirstLean/algebraicgeometry/Algebraicgeometry/Chapter2.lean
@@ -20,8 +20,9 @@ def affineVariety (Funcs : Set (MvPolynomial σ K)): Set (σ → K) :=
     {x : σ → K | ∀ p ∈ Funcs, p.eval x = 0}
 
 
+
 theorem isAffineVariety (I : Ideal (MvPolynomial σ K)):
-  ∃ funcs : Set (MvPolynomial σ K), MvPolynomial.zeroLocus K I = affineVariety funcs := by
+  ∃ funcs : Set (MvPolynomial σ K), funcs.Finite ∧ MvPolynomial.zeroLocus K I = affineVariety funcs := by
   have finitely_gen: I.FG := by --- Hilbert Basis theorem holds as MvPolynomial σ K a Noetherian ring
     apply isNoetherian_def.1
     apply MvPolynomial.isNoetherianRing
@@ -32,37 +33,28 @@ theorem isAffineVariety (I : Ideal (MvPolynomial σ K)):
     exact finitely_gen
   rcases fin_basis with ⟨f, fin, span⟩ --- Extract the finite basis
   use f
-  ext x
   constructor
-  · intro h p hp --- Get MvPolynomial p that we need to evaluate to 0 at x
-    apply h --- Suffices to show it is in the ideal as all functions in here are 0 at x
-    subst span --- I = <f1,...,fs> so showing p in the ideal equivalent to showing it is in the set spanned by these fi
-    apply Submodule.mem_span_of_mem --- p is actually a member of the generating set and this is by definition in the set spanned
-    exact hp
-  · intro h p hp
-    subst span  --- I = <f1,...,fs>
-    --- This gives us that p ∈ I = <f1,...,fs> means that it is a linear combination of these generators
-    have in_span: ∃ (l : ↑f →₀ MvPolynomial σ K), (Finsupp.linearCombination (MvPolynomial σ K) Subtype.val) l = p := by
-      apply (Finsupp.mem_span_iff_linearCombination (MvPolynomial σ K) (f) (p)).1
+  · exact fin
+  · ext x
+    constructor
+    · intro h p hp --- Get MvPolynomial p that we need to evaluate to 0 at x
+      apply h --- Suffices to show it is in the ideal as all functions in here are 0 at x
+      subst span --- I = <f1,...,fs> so showing p in the ideal equivalent to showing it is in the set spanned by these fi
+      apply Submodule.mem_span_of_mem --- p is actually a member of the generating set and this is by definition in the set spanned
       exact hp
-    rcases in_span with ⟨coeffs, is_lin_comb⟩ --- Extract the coefficients
-    subst is_lin_comb
-    have eval_zero : ∀ f' ∈ f, (MvPolynomial.eval x) f' = 0 := by
-      intro f' hf'
-      apply h; exact hf'
-    rw [Finsupp.linearCombination_apply (MvPolynomial σ K) (coeffs)] --- Writes the linear combination as an actual linear combination
-    
+    · intro h p hp
+      subst span  --- I = <f1,...,fs>
+      --- This gives us that p ∈ I = <f1,...,fs> means that it is a linear combination of these generators
+      have in_span: ∃ (l : ↑f →₀ MvPolynomial σ K), (Finsupp.linearCombination (MvPolynomial σ K) Subtype.val) l = p := by
+        apply (Finsupp.mem_span_iff_linearCombination (MvPolynomial σ K) (f) (p)).1
+        exact hp
+      rcases in_span with ⟨coeffs, is_lin_comb⟩ --- Extract the coefficients
+      subst is_lin_comb
+      have eval_zero : ∀ f' ∈ f, (MvPolynomial.eval x) f' = 0 := by
+        intro f' hf'
+        apply h; exact hf'
+      rw [Finsupp.linearCombination_apply (MvPolynomial σ K) (coeffs)] --- Writes the linear combination as an actual linear combination
+      simp only [Finsupp.sum, smul_eq_mul, map_sum, MvPolynomial.aeval_eq_eval, map_mul,
+        Subtype.coe_prop, eval_zero, mul_zero, Finset.sum_const_zero]
 
-
-
-
-
-
-
-
-
-
-#check Finsupp.linearCombination
-#check Finsupp.mem_span_iff_linearCombination
-#check Submodule.fg_def
-#check Finsupp.coe_eq_zero
+end AlgebraicGeometry
